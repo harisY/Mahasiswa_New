f676f7a59e6ad96738205f3e5b25f9c7497147e0
Nandita Putra P <Nandita Putra P@DESKTOP-E5UURAI>
2019-11-03 13:42:46 +0700

    tambah komen

--- Mahasiswa_New/Controllers/HomeController.cs
+++ Mahasiswa_New/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Mahasiswa_New.Controllers
         {
             return View();
         }
-
+        //Tambah Komen
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
